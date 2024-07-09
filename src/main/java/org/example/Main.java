b060927733be8cbe33d9591e9202e6f1a25c9a3e
zackharshal <harshalgotarne25@gmail.com>
2024-07-09 15:16:43 +0530

    Last name
    Checked if the last name has more than 2 characters and the first character is capital

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,12 +9,23 @@ public class Main {
         System.out.print("Enter first name: ");
         String firstName = scanner.next();
         String firstNamePattern = "^[A-Z][a-zA-Z]{2,}$";
-        Pattern compiledPattern = Pattern.compile(firstNamePattern);
-        Matcher matcher = compiledPattern.matcher(firstName);
-        if(matcher.matches()){
+        Pattern compiledPattern1 = Pattern.compile(firstNamePattern);
+        Matcher matcher1 = compiledPattern1.matcher(firstName);
+        if(matcher1.matches()){
             System.out.println("Valid first name.");
         }else{
             System.out.println("Invalid first name.");
+        }
+
+        System.out.print("Enter first name: ");
+        String lastName = scanner.next();
+        String lastNamePattern = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern compiledPattern2 = Pattern.compile(lastNamePattern);
+        Matcher matcher2 = compiledPattern2.matcher(lastName);
+        if(matcher2.matches()){
+            System.out.println("Valid last name.");
+        }else{
+            System.out.println("Invalid last name.");
         }
     }
 }
